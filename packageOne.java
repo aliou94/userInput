8555bfdebd8f9b6062c74ae8c4144e4a0ddbae86
ALI <ahmedaliou94@gmail.com>
2021-10-04 19:56:48 -0400

    first message

---
+++ packageOne.java
@@ -0,0 +1,19 @@
+package myPackage;
+
+public class packageOne {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 0;
+		
+		while(i <= 20) {
+             
+			System.out.println(i);
+			i++;
+			
+		}
+
+}
+
+}
