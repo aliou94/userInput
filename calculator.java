36f61846f8a6f1eab2a573db559af12cce4efaa6
ALI <ahmedaliou94@gmail.com>
2021-10-04 21:46:38 -0400

    'calculator'

---
+++ calculator.java
@@ -0,0 +1,67 @@
+package myPackage;
+
+import java.util.Scanner;
+
+public class calculator {
+	
+	
+	static void add(int a, int b) {
+		
+	System.out.print(a+b);
+		
+	}
+	
+static void subtract(int a, int b) {
+		
+	System.out.print(a-b);
+		
+	}
+
+
+static void divde (int a, int b) {
+	
+	System.out.print(a/b);
+	
+}
+
+static void  multiply (int a, int b) {
+	
+	System.out.print(a*b);;
+	
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner myObj = new Scanner(System.in); // Create a Scanner object
+		System.out.println("Enter a first number");
+		
+		 int num1 = myObj.nextInt(); // Read user input
+
+		 System.out.println("Enter a second number");
+		 int num2 = myObj.nextInt();
+		 
+		 System.out.println("Enter an operation");
+		 String operation = myObj.next();
+		 
+		 
+		 switch(operation.toUpperCase()) {
+		  case "ADD":
+		      add(num1, num2);
+		    break;
+		  case "SUBTRACT":
+		       subtract(num1, num2);
+		    break;
+		  case "Multiply":
+			  multiply (num1,num2);
+			    break;
+		  case "Divide":
+			  divde(num1, num2);
+			    break;
+		  default:
+		    // code block
+		}
+
+	}
+
+}
